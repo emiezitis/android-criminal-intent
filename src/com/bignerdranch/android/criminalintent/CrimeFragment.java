d059e87a52712cafa612b94b74134e03fdb2c00d
Wiggert <wloonstra@ebay.com>
2013-08-02 12:48:43 +0200

    353 Send a crime report via an implicit intent, with a chooser always (if >1 applicable app)

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -135,6 +135,19 @@ public class CrimeFragment extends Fragment {
             mPhotoButton.setEnabled(false);
         }
 
+        Button reportButton = (Button) v.findViewById(R.id.crime_reportButton);
+        reportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
+                i = Intent.createChooser(i, getString(R.string.send_report));
+                startActivity(i);
+            }
+        });
+
         return v;
     }
 
