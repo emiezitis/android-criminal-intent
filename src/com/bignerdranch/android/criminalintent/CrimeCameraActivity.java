7095267407608e1921c5713b92b704c285b62045
Wiggert <wloonstra@ebay.com>
2013-08-02 09:25:37 +0200

    316 Hide status bar and action bar when camera was activated.

--- src/com/bignerdranch/android/criminalintent/CrimeCameraActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeCameraActivity.java
@@ -1,8 +1,24 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CrimeCameraActivity  extends SingleFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Hide the window title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // Hide the status bar and other OS-level chrome
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // WL after the above changes, call super.onCreate
+        // The above changes need to be done before setContentView which
+        // is called in the superclass
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     protected Fragment createFragment() {
